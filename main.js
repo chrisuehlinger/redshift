c90f7529905b9d6b263a4b6a7c4fda13d06705a3
Chris Uehlinger <chris.uehlinger@gmail.com>
2015-10-11 19:27:34 -0400

    Got redshift to center on the mouse

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-var width = 960,
-    height = 500;
+var width = window.outerWidth,
+    height = window.outerHeight;
 
-var randomX = d3.random.normal(width / 2, 80),
-    randomY = d3.random.normal(height / 2, 80);
+var randomX = d3.random.normal(width / 2, 100),
+    randomY = d3.random.normal(height / 2, 100);
 
-var data = d3.range(2000).map(function() {
+var data = d3.range(1000).map(function() {
   return [
     randomX(),
     randomY()
@@ -19,11 +19,15 @@ var y = d3.scale.linear()
     .domain([0, height])
     .range([height, 0]);
 
-var svg = d3.select("body").append("svg")
+var color = d3.scale.linear()
+    .domain([0, Math.max(width, height) / 3.5, Math.max(width, height) / 1.5])
+    .range(['white', 'red', 'black']);
+
+var svg = d3.select("svg")
     .attr("width", width)
     .attr("height", height)
   .append("g")
-    .call(d3.behavior.zoom().x(x).y(y).scaleExtent([1, 8]).on("zoom", zoom));
+    .call(d3.behavior.zoom().x(x).y(y).scaleExtent([1, 10]).on("zoom", zoom));
 
 svg.append("rect")
     .attr("class", "overlay")
@@ -33,11 +37,21 @@ svg.append("rect")
 var circle = svg.selectAll("circle")
     .data(data)
   .enter().append("circle")
-    .attr("r", 2.5)
-    .attr("transform", transform);
+    .attr("r", 3);
+
+svg.on("mousemove", zoom);
+zoom();
 
 function zoom() {
-  circle.attr("transform", transform);
+  var mouseCoords = d3.mouse(document.body);
+    
+  circle
+      .attr("transform", transform)
+      .attr("fill", function(d){ var c = [x(d[0]), y(d[1])]; return color(distance(c, mouseCoords)); });
+}
+
+function distance(c1, c2){
+    return Math.sqrt((c2[1] - c1[1])*(c2[1] - c1[1]) + (c2[0] - c1[0])*(c2[0] - c1[0]));
 }
 
 function transform(d) {
