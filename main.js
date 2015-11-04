01da2383bec2a730adcd7e5ccac32d4f87b70d19
Chris Uehlinger <chris.uehlinger@gmail.com>
2015-11-04 00:16:07 -0500

    Got it all working! Sort of

--- main.js
+++ main.js
@@ -1,15 +1,29 @@
 var width = window.outerWidth,
     height = window.outerHeight;
 
-var randomX = d3.random.normal(width / 2, 100),
-    randomY = d3.random.normal(height / 2, 100);
+var randomX = d3.random.normal(width / 2, width / 5),
+    randomY = d3.random.normal(height / 2, height / 5);
 
-var data = d3.range(1000).map(function() {
-  return [
+var data = d3.range(1000).map(function () {
+    return [
     randomX(),
     randomY()
   ];
 });
+
+var ship = {
+    position: {
+        x: width / 2,
+        y: height / 2
+    },
+    velocity: {
+        x: 0,
+        y: 0
+    },
+    direction: 0
+};
+
+var speedOfLight = 3;
 
 var x = d3.scale.linear()
     .domain([0, width])
@@ -20,40 +34,130 @@ var y = d3.scale.linear()
     .range([height, 0]);
 
 var color = d3.scale.linear()
-    .domain([0, Math.max(width, height) / 3.5, Math.max(width, height) / 1.5])
-    .range(['white', 'red', 'black']);
+    .domain([-Math.PI / 2, 0, Math.PI / 2])
+    .range(['red', 'white', 'blue']);
 
 var svg = d3.select("svg")
     .attr("width", width)
     .attr("height", height)
-  .append("g")
-    .call(d3.behavior.zoom().x(x).y(y).scaleExtent([1, 10]).on("zoom", zoom));
+    .append("g")
+    //.call(d3.behavior.zoom().x(x).y(y).scaleExtent([1, 10]).on("zoom", render));
 
 svg.append("rect")
     .attr("class", "overlay")
     .attr("width", width)
     .attr("height", height);
 
-var circle = svg.selectAll("circle")
+var star = svg.selectAll("circle.star")
     .data(data)
-  .enter().append("circle")
+    .enter().append("circle")
+    .attr('class', 'star')
     .attr("r", 3);
 
-svg.on("mousemove", zoom);
-zoom();
+var shipSprite = svg.selectAll('circle.ship')
+    .data([JSON.parse(JSON.stringify(ship))]);
 
-function zoom() {
-  var mouseCoords = d3.mouse(document.body);
-    
-  circle
-      .attr("transform", transform)
-      .attr("fill", function(d){ var c = [x(d[0]), y(d[1])]; return color(distance(c, mouseCoords)); });
+shipSprite
+    .enter().append('circle')
+    .attr('class', 'ship')
+    .attr('r', 5)
+    .attr('fill', 'green');
+
+function render() {
+    star
+        .attr('transform', function (d, i) {
+            var c = [d[0], d[1]];
+            var dist = distance(c, [ship.position.x, ship.position.y]);
+            var lorentz = 1 / Math.sqrt(1 - Math.pow(ship.totalSpeed / speedOfLight, 2));
+            var stretchDist = dist*lorentz;
+        
+        
+            var starDirection = Math.atan2(ship.position.y - c[1], ship.position.x - c[0]);
+            var angleDifference = ship.direction - starDirection;
+            angleDifference *= (180 / Math.PI);
+            angleDifference = (angleDifference + 180) % 360 - 180;
+            angleDifference *= (Math.PI / 180);
+            var stretchAngle = Math.atan2(speedOfLight * Math.sin(angleDifference), ship.totalSpeed + speedOfLight * Math.cos(angleDifference));
+        
+            var stretchX = ship.velocity.x > 0 ? ship.position.x - Math.cos(stretchAngle)*stretchDist :  ship.position.x + Math.cos(stretchAngle)*stretchDist;
+            var stretchY = ship.velocity.x < 0 ? ship.position.y - Math.sin(stretchAngle)*stretchDist : ship.position.y + Math.sin(stretchAngle)*stretchDist;
+        
+            if (i === 0) {
+                console.log(Math.round(d[0]),Math.round(stretchX), Math.round(d[1]), Math.round(stretchY));
+                //console.log(Math.round(100*ship.velocity.total / speedOfLight)/100, lorentz, Math.round(180*ship.direction/Math.PI), Math.round(180*stretchAngle/Math.PI));
+            }
+        
+            var starX = x(stretchX);
+            var starY = y(stretchY);
+            return "translate(" + starX + "," + starY + ")";
+        })
+        .attr("fill", function (d, i) {
+            var c = [d[0], d[1]];
+
+            var dist = distance(c, [ship.position.x, ship.position.y]);
+            var starDirection = Math.atan2(ship.position.y - c[1], ship.position.x - c[0]);
+
+            var angleDifference = ship.direction - starDirection;
+            angleDifference *= (180 / Math.PI);
+            angleDifference = Math.abs((angleDifference + 180) % 360 - 180) - 90;
+            angleDifference *= (Math.PI / 180);
+            var doppler = angleDifference * ship.totalSpeed / speedOfLight;
+//            if (i === 0) {
+//                console.log(d[0], d[1], ship.position.x, ship.position.y, Math.round(100 * ship.direction / Math.PI) / 100, Math.round(100 * starDirection / Math.PI) / 100, Math.round(100 * angleDifference / Math.PI) / 100);
+//            }
+            if (i === 0) return 'green';
+        
+            var normalColor = color(doppler);
+            var brightnessAdjustedColor = d3.hsl(normalColor)
+            if(doppler < 0) brightnessAdjustedColor = brightnessAdjustedColor.darker(-dist*doppler/50);
+            return brightnessAdjustedColor;
+        });
+
+    shipSprite
+        .attr('transform', function (d) {
+            return transform([ship.position.x, ship.position.y]);
+        });
 }
 
-function distance(c1, c2){
-    return Math.sqrt((c2[1] - c1[1])*(c2[1] - c1[1]) + (c2[0] - c1[0])*(c2[0] - c1[0]));
+function distance(c1, c2) {
+    return Math.sqrt((c2[1] - c1[1]) * (c2[1] - c1[1]) + (c2[0] - c1[0]) * (c2[0] - c1[0]));
 }
 
 function transform(d) {
-  return "translate(" + x(d[0]) + "," + y(d[1]) + ")";
+    var starX = x(d[0]);
+    var starY = y(d[1]);
+    return "translate(" + starX + "," + starY + ")";
 }
+
+$(window).on('keypress', function (e) {
+    //    console.log(e.which);
+    var thrust = 0.01;
+    if (e.which === 115) {
+        ship.velocity.y -= thrust;
+    }
+    if (e.which === 119) {
+        ship.velocity.y += thrust;
+    }
+    if (e.which === 97) {
+        ship.velocity.x -= thrust;
+    }
+    if (e.which === 100) {
+        ship.velocity.x += thrust;
+    }
+});
+
+d3.timer(function () {
+    ship.direction = Math.atan2(ship.velocity.y, ship.velocity.x);
+    ship.totalSpeed = Math.sqrt(Math.pow(ship.velocity.x, 2) + Math.pow(ship.velocity.y, 2));
+    if(ship.totalSpeed > speedOfLight){
+        ship.totalSpeed = speedOfLight;
+        ship.velocity.x = Math.cos(ship.direction)*ship.totalSpeed;
+        ship.velocity.y = Math.sin(ship.direction)*ship.totalSpeed;
+    }
+
+
+    ship.position.x += ship.velocity.x;
+    ship.position.y += ship.velocity.y;
+
+    render();
+});
